6246bf130b4c78ee73fa9ddc7a770da80842714b
Sanjana Choudhari <sanjanachoudahrimarch@gmail.com>
2026-08-21 18:48:10 +0530

    Operators.js 14/8/2026

---
+++ operators.js
@@ -0,0 +1,29 @@
+let x = 5;
+console.log("Addition",x+5,"Subtraction",x-5,
+    "Multiplication",x*5,"division",x/5
+);
+console.log("postincrement",x++);
+let val=10;
+console.log("pre decrement", --val);
+
+//Strings pe operation
+let firstname="Sachin";
+let lastname="Tendulkar";
+let fullname=firstname+lastname;
+console.log(fullname);
+
+console.log(val);
+console.log("Exponential value ** 3",val**3);
+
+//Assignment Operators
+let all=7;
+console.log(all+=5);
+console.log(all-=5);
+console.log(all*=5);
+
+//Comparision operator //Strictly equal operator
+console.log("Strictly equal",2!==2, "is",2!=='2');
+
+//Logical operators
+/*let x=3;
+console.log((x<5)) //Sir sent ig*/
