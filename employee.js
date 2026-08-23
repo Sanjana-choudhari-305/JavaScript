ad45d758338a310cfd34c0c904d96185a85c6280
Sanjana-choudhari-305 <choudharisanjana305@gmail.com>
2026-08-23 20:16:07 +0530

    Tekstac Assignment 1
    Added a program to store and display employee information with various data types.

---
+++ employee.js
@@ -0,0 +1,23 @@
+/*Description
+Objective:
+To work with variables and datatypes
+
+Scenario:
+Create a program to store and display information about an employee.
+The program should demonstrate the use of different variable types and print the values in a readable format.
+
+Sample output:
+Employee Information:
+Name: Ravi Kumar
+Age: 28
+Permanent Employee: Yes
+Skills: JavaScript, HTML, CSS*/
+
+let Name="Ravi Kumar";
+let Age = 28;
+let PermanentEmployee = true;
+let Skills = ["JavaScript", "HTML", "CSS"];
+console.log("Name: "+Name);
+console.log("Age: "+Age);
+console.log("Permanent Employee: "+(PermanentEmployee?"Yes":"No"));
+console.log("Skills: "+Skills);
